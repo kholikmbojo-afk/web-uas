88d312dec139ccb9fd5a0f9bef25d20e19184043
kholikmbojo-afk <kholikmbojo@gmail.com>
2026-06-05 18:05:24 +0700

    h

--- web-dinamis/includes/header.php
+++ web-dinamis/includes/header.php
@@ -5,6 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FIFA World Cup 2026</title>
 
-    <link rel="stylesheet" href="/worldcup2026/assets/css/style.css">
+    <link rel="stylesheet" href="/assets/css/style.css">
 </head>
 <body>
